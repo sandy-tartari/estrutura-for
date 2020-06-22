000c49a1f3443ec5481a6da4696f28cc8dfd11c4
sandy-tartari <62676992+sandy-tartari@users.noreply.github.com>
2020-06-22 17:02:09 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ContadeUmAteDez
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        // Faça um algoritmo, usando um método de repetição, que mostre os números de 1 até 10.
+        for (int numero = 1; numero <= 10; numero++) {
+            Console.WriteLine(numero);
+        }        
+        }
+    }
+}
